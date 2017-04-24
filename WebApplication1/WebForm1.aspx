9036267cd40add6d41b645c98075c2ffed946ed1
ibk <itamarbk@gmail.com>
2017-04-24 15:33:51 +0300

    sinup

---
+++ WebApplication1/WebForm1.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>
+    </title>
+    <script>
+        function func() {
+            if (signup["password"] != signup["password2"]) {
+                alert("the passwords do not match");
+                return false;
+            }
+            else
+            return true;
+        }
+    </script>
+</head>
+<body>
+    <form id="signup" runat="server" onsubmit="func();">
+    <div>
+        username <input type="text" name="username" /><br />
+        password <input type="text" name="password" /><br />
+        re-type password <input type="text" name="password2" /><br />
+        city <input type="text" name="city" /><br />
+        age <input type="number" name="age" /><br />
+        status <input type="text" name="status" /><br />
+        <input type="submit" name ="submit" /><br />
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        private string connstring = "Data Source=.\\SQLEXPRESS;AttachDbFilename=\"c:\\users\\user1\\documents\\visual studio 2010\\Projects\\WebApplication1\\WebApplication1\\App_Data\\Database1.mdf\";Integrated Security=True;User Instance=True";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request["onsubmit"] == null)
+            {
+                if (Request["submit"] != null)
+                {
+                    SqlConnection conn = new SqlConnection(connstring);
+                    conn.Open();
+                    SqlCommand cmd = conn.CreateCommand();
+                    //cmd.CommandText = "SELECT username FROM users WHERE username='" + Request["username"] + "';";
+                    //if (cmd.ExecuteReader() == null||true)
+                   /* {
+                        cmd.CommandText = "INSERT INTO users (username, password, city, age, status) VALUES ('" + Request["username"] + "', '" + Request["password"] + "', '" + Request["city"] + "', '" + int.Parse(Request["age"]) + "', '" + Request["status"] + "');";
+                        cmd.ExecuteNonQuery();
+                    }*/
+                   // else
+                        Response.Write("<h1>this username is taken, try again</h1>");
+                    conn.Close();
+
+                }
+            }
+        }
+    }
+}
