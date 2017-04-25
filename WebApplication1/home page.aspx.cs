8b487568099f066ad89c2bf2ebc72eb5b8f37289
itamarbk <itamarbk@gmail.com>
2017-04-25 23:21:04 +0300

    confirm password
    confirms password, does not validate that the username is taken

--- WebApplication1/home page.aspx.cs
+++ WebApplication1/home page.aspx.cs
@@ -18,21 +18,13 @@ namespace WebApplication1
             conn.Open();
             SqlCommand cmd = conn.CreateCommand();
             cmd.CommandText = "SELECT * FROM users;";
-            SqlDataReader rdr2 = cmd.ExecuteReader();
-            while (rdr2.Read())
+            SqlDataReader rdr = cmd.ExecuteReader();
+            while (rdr.Read())
             {
-                {
-                    SqlCommand cmd2 = conn.CreateCommand();
-                        cmd2.CommandText = "Select * From users WHERE username='" + rdr2["username"] + "';";
-                    //SqlDataReader rdr2 = cmd2.ExecuteReader();
-                    while (rdr2.Read())
-                    {
-                        MyList += "<p><font size=\"4\" ><b>" + rdr2["username"].ToString() + "</b></font>";
-                        MyList += "<br/>age:" + rdr2["age"];
-                        MyList += "<br/>city:" + rdr2["city"];
-                        MyList += "<br/><font size=\"4\" ><b>" + rdr2["status"] + "</b></font></p>";
-                    }
-                }
+                MyList += "<p><font size=\"4\" ><b>" + rdr["username"].ToString() + "</b></font>";
+                MyList += "<br/>age:" + rdr["age"];
+                MyList += "<br/>city:" + rdr["city"];
+                MyList += "<br/><font size=\"5\" ><b>" + rdr["status"] + "</b></font></p>";
             }
             conn.Close();
         }
--- WebApplication1/login.aspx
+++ WebApplication1/login.aspx
@@ -4,7 +4,7 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title></title>
+    <title>Impress Me Login</title>
     <link rel="Stylesheet" href="StyleSheet1.css" type="text/css" />
 </head>
 <body class="StyleSheet1.css">
